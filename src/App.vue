3edddb560e9f33eb45cb15011730455b34af98bc
antoniodelgiudice02 <antodelgiu02@gmail.com>
2024-04-14 21:47:42 +0200

    milestone 5

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,25 @@ import axios from 'axios';
 export default {
     data() {
         return {
-            title: 'Boolfolio'
+            title: 'Boolfolio',
+            projects: [],
         }
     },
 
     created() {
-        axios.get('http://127.0.0.1:8000/api/projects').then((response) => {console.log(response.data.data)});
-    }
+        axios.get('http://127.0.0.1:8000/api/projects').then((response) => { this.projects = response.data.data; console.log(response.data.data) });
+    },
+
+    // methods() {
+    //     function getContent(n_char, text) {
+    //         if(strlen(text) > n_char){
+    //             return substr(text, 0, n_char) + '...';
+    //         }
+    //         else{
+    //             return text;
+    //         }
+    //     }
+    // }
 }
 
 
@@ -22,6 +34,23 @@ export default {
 
     <div class="container">
         <h1>{{ title }}</h1>
+        <div class="row">
+            <div v-for='project in projects' class="col-3">
+                <div class="card">
+                    <div class="card-header">{{ project.title }}</div>
+                    <div class="card-body">
+                        <ul>
+                            <li>
+                                <strong>Id:</strong><span>{{ project.id }}</span>
+                            </li>
+                            <li>
+                                <strong>Descrizione:</strong><span>{{ project.content }}</span>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 
 
