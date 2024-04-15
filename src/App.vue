1f039f6143ab2e378bd1d2891524c1385b6a5541
antoniodelgiudice02 <antodelgiu02@gmail.com>
2024-04-15 15:54:25 +0200

    add components

--- src/App.vue
+++ src/App.vue
@@ -2,28 +2,28 @@
 
 import axios from 'axios';
 
+//store
+import { api, store } from './store';
+
+//componenti
+import AppHeader from './components/layout/AppHeader.vue';
+import ProjectsList from './components/layout/main/ProjectsList.vue';
+
 export default {
     data() {
         return {
-            title: 'Boolfolio',
-            projects: [],
+            store,
+            title: 'Boolfolio'
         }
     },
 
     created() {
-        axios.get('http://127.0.0.1:8000/api/projects').then((response) => { this.projects = response.data.data; console.log(response.data.data) });
+        axios.get(api.baseUrl + 'projects').then((response) => {
+            store.projects = response.data.data;
+        });
     },
 
-    // methods() {
-    //     function getContent(n_char, text) {
-    //         if(strlen(text) > n_char){
-    //             return substr(text, 0, n_char) + '...';
-    //         }
-    //         else{
-    //             return text;
-    //         }
-    //     }
-    // }
+    components: { AppHeader, ProjectsList }
 }
 
 
@@ -32,26 +32,9 @@ export default {
 
 <template>
 
-    <div class="container">
-        <h1>{{ title }}</h1>
-        <div class="row">
-            <div v-for='project in projects' class="col-3">
-                <div class="card">
-                    <div class="card-header">{{ project.title }}</div>
-                    <div class="card-body">
-                        <ul>
-                            <li>
-                                <strong>Id:</strong><span>{{ project.id }}</span>
-                            </li>
-                            <li>
-                                <strong>Descrizione:</strong><span>{{ project.content }}</span>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    <AppHeader :title="title"></AppHeader>
+
+    <ProjectsList></ProjectsList>
 
 
 </template>
---
+++ src/components/layout/main/ProjectsList.vue
@@ -0,0 +1,45 @@
+<script>
+
+//store
+import { api, store } from '../../../store';
+
+export default {
+    data() {
+        return {
+            store,
+            title: 'Projects',
+        }
+    },
+}
+
+</script>
+
+<template>
+
+    <div class="container">
+        <h1>{{ title }}</h1>
+        <div class="row">
+            <div v-for='project in store.projects' class="col-4 my-2">
+                <div class="card">
+                    <div class="card-header">{{ project.title }}</div>
+                    <div class="card-body">
+                        <ul>
+                            <li>
+                                <strong>Id:</strong><span>{{ project.id }}</span>
+                            </li>
+                            <li>
+                                <strong>Descrizione:</strong><span>{{ project.content }}</span>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+</template>
+
+<style lang="scss">
+@use '/src/scss/general.scss'
+</style>
